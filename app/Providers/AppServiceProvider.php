b895e64e2df90f3863c193aac8696b34f0cb8a17
lucas <lucas.lebon75@gmail.com>
2025-12-29 17:04:00 +0100

    fix: try fix documentation, add token while it's necessary

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,14 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\Operation;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Dedoc\Scramble\Support\RouteInfo;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +27,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        Scramble::configure()
+            ->withDocumentTransformers(function (OpenApi $openApi) {
+                $openApi->secure(
+                    SecurityScheme::http('bearer')
+                );
+            })
+            ->withOperationTransformers(function (Operation $operation, RouteInfo $routeInfo) {
+                $routeMiddleware = $routeInfo->route->gatherMiddleware();
+
+                $hasAuthMiddleware = collect($routeMiddleware)->contains(
+                    fn ($m) => Str::startsWith($m, 'auth:')
+                );
+
+                if (!$hasAuthMiddleware) {
+                    $operation->security = [];
+                }
+            });
     }
 }
